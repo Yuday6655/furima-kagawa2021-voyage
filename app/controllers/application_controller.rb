ba15ff9f4ad15e9587f993f996dc0c698bc1f010
Yuday6655 <youdayxx@i.softbank.jp>
2021-10-08 19:56:35 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      end
+    end
+  end
 end
