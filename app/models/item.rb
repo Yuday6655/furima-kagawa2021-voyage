73b08277ca80afe4d0f7210a4055bc28b13f306c
hiroops <89205167+hiroops@users.noreply.github.com>
2021-10-11 19:52:34 +0900

    Update item.rb
    association, validations追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :quality
+  belongs_to_active_hash :Shipping_fee
+  belongs_to_active_hash :Prefecture
+  belongs_to_active_hash :Shipment_date
+
+ validates :title, :detail :price, presence: true
+ validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+
+ validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+ validates :quality_id, numericality: { other_than: 1, message: "can't be blank" } 
+ validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" } 
+ validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+ validates :shipment_date_id, numericality: { other_than: 1, message: "can't be blank" } 
+
+ validates :price, numericality: true
+ 
 end
