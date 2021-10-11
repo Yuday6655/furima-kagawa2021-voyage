68455de27581d584ef9cdbe6271bfd4ece228369
Yuday6655 <youdayxx@i.softbank.jp>
2021-10-11 17:32:25 +0900

    エラーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  begore_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, key: [:nickname, :first_name_kanji, :last_name_kanji, :first_name_kana, :last_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name_kanji, :last_name_kanji, :first_name_kana, :last_name_kana, :birth_date])
   end
 end
