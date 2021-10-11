ea92036c93010a2b9885a9af6834e2f213007554
Yuday6655 <youdayxx@i.softbank.jp>
2021-10-11 16:48:02 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :orders
+
+  validates :nickname, presence: true
+  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :birth_date, presence: true
+
 end
