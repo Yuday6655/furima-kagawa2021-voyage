661e3a61c2fddc5f81ca6b6b2a2044865dc9ecc3
hiroops <89205167+hiroops@users.noreply.github.com>
2021-10-14 08:24:28 +0900

    itemsテーブルにuser_id導入等

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,15 +7,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipment_date
 
- validates :image, presence: true
- validates :title, :detail, :price, presence: true
+with_options presence: true do
+ validates :image
+ validates :title
+ validates :detail
  validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+end
 
- validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
- validates :quality_id, numericality: { other_than: 1, message: "can't be blank" } 
- validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" } 
- validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
- validates :shipment_date_id, numericality: { other_than: 1, message: "can't be blank" } 
+with_options numericality: { other_than: 1, message: "can't be blank" } do
+ validates :category_id
+ validates :quality_id
+ validates :shipping_fee_id
+ validates :prefecture_id
+ validates :shipment_date_id
+end
 
  validates :price, numericality: true
 
--- db/migrate/20211011015151_create_items.rb
+++ db/migrate/20211011015151_create_items.rb
@@ -1,6 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.references :user,        null: false, foreign_key: true
       t.string :title,           null:false
       t.text  :detail,           null:false
       t.integer :category_id,      null:false
