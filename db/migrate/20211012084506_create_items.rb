9ec0751019651359c67d06b31a440c469e720929
muguchan <naoko.unsui@gmail.com>
2021-10-14 20:54:19 +0900

    itemsテーブルの削除

--- db/migrate/20211012084506_create_items.rb
+++
@@ -1,8 +0,0 @@
-class CreateItems < ActiveRecord::Migration[6.0]
-  def change
-    create_table :items do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_10_12_084506) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_10_11_044642) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
