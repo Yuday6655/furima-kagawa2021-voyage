7edebec9d14b01b9160f5ee059fc1d2c21947759
muguchan <naoko.unsui@gmail.com>
2021-10-15 19:49:06 +0900

    imageparamsの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
 
  private
   def item_params
-    params.require(:item).permit(:title, :detail, :image, :category_id, :quality_id, :shipping_fee_id, :prefecture_id, :shipment_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :detail, :image, :category_id, :quality_id, :shipping_fee_id, :prefecture_id, :shipment_date_id, :price, :image).merge(user_id: current_user.id)
   end 
 end
