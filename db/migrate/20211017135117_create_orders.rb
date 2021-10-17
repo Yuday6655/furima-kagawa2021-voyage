f46a3272e3677c6a3314e180c521b6b633786477
Yoshmong <arietta44@gmail.com>
2021-10-17 23:22:20 +0900

    ordersテーブルを作成

--- db/migrate/20211017135117_create_orders.rb
+++ db/migrate/20211017135117_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.references :user  ,null: false, foreign_key: true
+      t.references :item  ,null: false, foreign_key: true
       t.timestamps
     end
   end
