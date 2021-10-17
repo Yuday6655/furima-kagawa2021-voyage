082cc95b3daa90ba54f3a5797d87baa2af487867
Yoshmong <arietta44@gmail.com>
2021-10-17 20:26:20 +0900

    Showのビューファイル挿入、Rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all.order(id: "DESC")
+    @items = Item.all.order(id: 'DESC')
   end
 
   def new
