27da99d54eff67bc1619af7a9e3917c8a67d325b
Yoshmong <arietta44@gmail.com>
2021-10-19 07:31:22 +0900

    購入画像表示（orderビュー）修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,25 @@
 class OrdersController < ApplicationController
 
-before_action :set_order, :authenticate_user!
+before_action :set_item, :authenticate_user!
 
 def index
+  @order = Order.new
 end
 
  def create
- end
+  @order = Order.new(order_params)
+  if @order.valid?
+    @order.save
+    return redirect_to root_path
+  else
+    render 'index'
+  end
+end
 
 
 
 private
- def set_order
+ def set_item
     #購入品の情報の取得
     @item = Item.find(params[:item_id])
 
@@ -19,4 +27,9 @@ private
     #   redirect_to item_path(@item.id), alert: '売り切れのため、購入できません'
     # end
   end
+
+  def order_params     #price ではなく、カード情報などが入る？
+    params.require(:order).permit(:price) .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
