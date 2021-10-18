b0332461c3136becf0708d9fd49aff474ff91bbc
Yoshmong <arietta44@gmail.com>
2021-10-18 22:17:01 +0900

    ビュー（購入内容確認、カード入力、住所入力）の分割

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
-  def index
-    @order = Order.new
-  end
 
-  def create      
-  end
+before_action :set_order, :authenticate_user!
 
+def index
+end
+
+ def create
+ end
+
+
+
+private
+ def set_order
+    #購入品の情報の取得
+    @item = Item.find(params[:item_id])
+
+    # if @item.oder_id
+    #   redirect_to item_path(@item.id), alert: '売り切れのため、購入できません'
+    # end
+  end
 end
