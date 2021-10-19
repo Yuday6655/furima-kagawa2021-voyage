ae8e76ca6aa468db2628917bbca4241b6aca823d
hiroops <89205167+hiroops@users.noreply.github.com>
2021-10-19 21:31:30 +0900

    商品情報編集機能の実装
    edit.html.erb追加、編集
    show.html.erb編集
    items_controller編集
    routes.rb編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,14 +12,31 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+        redirect_to root_path
     else
-      render :new
+        render :new
     end
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+        redirect_to root_path
+    end
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+        redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
