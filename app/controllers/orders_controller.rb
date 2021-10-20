d67dd29cc4b8d46ab8bb5343fe97170a5409041e
Yoshmong <arietta44@gmail.com>
2021-10-20 11:13:25 +0900

    Formオブジェクトを用いた実装に修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,31 +3,15 @@ class OrdersController < ApplicationController
 before_action :set_item, :authenticate_user!
 
 def index
-  @order = Order.new
+  @order_address = OrderAddress.new
  end
 
  def create
-  @order = Order.create(order_params)
-  Address.create(address_params)
-  redirect_to root_path
-end
-
-
-private
- def set_item
-    @item = Item.find(params[:item_id])
-    # if @item.oder_id
-    #   redirect_to item_path(@item.id), alert: '売り切れのため、購入できません'
-    # end
-  end
+  @order_address = OrderAddress.new(order_params)
+ end
+ private
 
   def order_params
-    params.require(:user).permit(:user_id).merge(user_id: current_user.id)
-    params.require(:item).permit(:item_id).merge(item_id: @item_id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city_town, :address_number, :building, :phone_number ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-  
-  def address_params   
-      params.permit(:postal_code, :prefecture_id, :city_town, :address_number, :building, :phone_number ).merge(order_id: @order.id)
-  end
-
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,2 +1,16 @@
 class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :order_id, :postal_code, :prefecture_id, :city_town, :address_number, :building, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
+
+  def save
+    Order.create(:item_id, :user_id)
+    Address.create(:order_id, :postal_code, :prefecture_id, :city_town, :address_number, :building, :phone_number)
+  end
+
 end
