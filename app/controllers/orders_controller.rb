2a1e37aaf1cc403cba6a4eac757cc569811e4603
Yoshmong <arietta44@gmail.com>
2021-10-20 09:05:53 +0900

    ordersコントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,29 +7,27 @@ def index
  end
 
  def create
-  @order = Order.new(order_params)
-  if @order.valid?
-    @order.save
-    return redirect_to root_path
-  else
-    render 'index'
-  end
+  @order = Order.create(order_params)
+  Address.create(address_params)
+  redirect_to root_path
 end
 
 
 private
  def set_item
-    #購入品の情報の取得
     @item = Item.find(params[:item_id])
-
-
     # if @item.oder_id
     #   redirect_to item_path(@item.id), alert: '売り切れのため、購入できません'
     # end
   end
 
-  def order_params   
-    params.require(:order).permit(:postal_code, :prefecture_id, :city_town, :address_number, :building, :phone_number ) .merge(user_id: current_user.id, item_id: params[:item_id])
+  def order_params
+    params.require(:order).permit(:item_id)
+    params.require(:order).permit(:user_id).merge(user_id: current_user.id,)
+  end
+  
+  def address_params   
+      params.permit(:postal_code, :prefecture_id, :city_town, :address_number, :building, :phone_number ) .merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
