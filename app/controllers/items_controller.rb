0d25e0ec6bf3101cda902c7e11ecb875f4ff8f54
hiroops <89205167+hiroops@users.noreply.github.com>
2021-10-20 10:53:07 +0900

    コードの簡素化（修正）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-                :set_item
+  before_action :set_item
 
   def index
     @items = Item.all.order(id: 'DESC')
