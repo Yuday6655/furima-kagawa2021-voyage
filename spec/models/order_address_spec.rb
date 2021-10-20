4b5a48daa9d8baf14f45845dce32b710fa696580
Yoshmong <arietta44@gmail.com>
2021-10-20 16:51:57 +0900

    売り切れの時は　sold out　を表示

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '送付先アドレスの保存' do
+    before do
+      user = FactoryBot.create(:user)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+      end
+      it 'buildingは空でも保存できること' do
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      end
+      it 'prefectureを選択していないと保存できないこと' do
+      end
+    end
+  end
 end
