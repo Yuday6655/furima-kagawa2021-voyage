6d95d60718c137e41b3b574f10a042fe702aadc3
Yoshmong <arietta44@gmail.com>
2021-10-21 20:31:12 +0900

    ストロングパラメーター編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,8 +10,8 @@ def create
    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
-        redirect_to root_path
-      else
+      return redirect_to root_path
+    else
         render :index
     end
 end
@@ -24,7 +24,7 @@ end
   end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city_town, :address_number, :building, :phone_number ).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city_town, :address_number, :building, :phone_number ).merge(user_id: current_user.id, item_id: params[:item_id], token: prams[:token])
   end
 
 
