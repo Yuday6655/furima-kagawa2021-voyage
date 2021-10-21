0ef521e37149290e79aa35b61b17eb0faefe701a
Yoshmong <arietta44@gmail.com>
2021-10-21 19:28:51 +0900

    js file　属性の修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,16 +3,17 @@ const pay = () => {
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    
+
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
+
     const card = {
-      number: formData.get("order[number]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
-      cvc: formData.get("order[cvc]"),
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
-    
+
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
