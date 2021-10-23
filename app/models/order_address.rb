e9e83fb04a5dfe1e2c7042b1758d510ad3b39799
Yoshmong <arietta44@gmail.com>
2021-10-23 22:05:30 +0900

    validation追記、商品売り切れの場合の条件分岐追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,10 @@ class OrderAddress
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :token, presence: true
+    validates :city_town
+    validates :address_number
+    validates :phone_number
+    validates :token
   end
 
   def save
