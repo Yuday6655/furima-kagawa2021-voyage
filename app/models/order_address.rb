b471f7dd384219688524f65aba395c862151dca4
Yoshmong <arietta44@gmail.com>
2021-10-26 12:18:29 +0900

    tem_idにバリデーション付与

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,6 +5,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id
+    validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city_town
